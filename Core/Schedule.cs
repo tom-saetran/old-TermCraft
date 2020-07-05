58b17524595c4caf1ca6751712e2512b253812b0
Zingo Fox <tom.saetran@gmail.com>
2020-07-05 13:27:25 +0200

    commit 32 (Den)

---
+++ Core/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+/*
+ * Holds a timer and a list of tasks
+ * Executes task upon exiry
+ * 
+ */
+namespace TermCraft {
+	class Schedule {
+
+		Thread thread;
+		private List<Task> tasks = new List<Task>();
+		private void Tasks () {
+
+		}
+
+		private void AddTask (Task task) {
+			
+		}
+	}
+}
---
+++ Core/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace TermCraft {
+	class Task {
+		bool runInstantly = false;
+		long spawnTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+		long expiryTime;
+
+		/*  use timestamps to add tasks to an array, sort on insert
+		 *  taskthread counts time and executes tasks when they have 
+		 *  count past their expiry time
+		 *  
+		 */ 
+	}
+}
